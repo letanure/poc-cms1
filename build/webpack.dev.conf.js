c626a39e5b9f5bbe4bd8f164ef631550aa26a432
Luiz Tanure <letanure@gmail.com>
2017-12-28 15:52:16 +0100

    build(html): add watch / reload to pug templates

--- build/webpack.dev.conf.js
+++ build/webpack.dev.conf.js
@@ -5,6 +5,7 @@ const config = require('../config')
 const merge = require('webpack-merge')
 const baseWebpackConfig = require('./webpack.base.conf')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+const ReloadHtmlWebpackPlugin = require('reload-html-webpack-plugin');
 const FriendlyErrorsPlugin = require('friendly-errors-webpack-plugin')
 const portfinder = require('portfinder')
 
@@ -50,6 +51,7 @@ const devWebpackConfig = merge(baseWebpackConfig, {
       template: 'src/templates/index.pug',
       inject: true
     }),
+    new ReloadHtmlWebpackPlugin(),
   ]
 })
 
