2900d35b454bac775d2f7932f2a9301db66c32c7
Luiz Tanure <letanure@gmail.com>
2018-01-05 10:29:53 +0100

    feat: add new stores to modules index

--- src/store/modules/index.js
+++ src/store/modules/index.js
@@ -1,15 +1,21 @@
 import Assets from '@/store/modules/assets'
+import Channels from '@/store/modules/channels'
 import Collections from '@/store/modules/collections'
 import Forms from '@/store/modules/forms'
+import Languages from '@/store/modules/languages'
 import Page from '@/store/modules/page'
 import Pages from '@/store/modules/pages'
 import User from '@/store/modules/user'
+import Users from '@/store/modules/users'
 
 export default {
   Assets,
+  Channels,
   Collections,
   Forms,
+  Languages,
   Page,
   Pages,
   User,
+  Users,
 }
