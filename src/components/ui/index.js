a42fb0d32584fbb98f400794e1c800648e730b1c
Luiz Tanure <letanure@gmail.com>
2018-01-06 12:25:05 +0100

    feat: add component form field

--- src/components/ui/index.js
+++ src/components/ui/index.js
@@ -1,5 +1,6 @@
 import BreadcrumbUi from '@/components/ui/BreadcrumbUi'
 import ConfirmUi from '@/components/ui/ConfirmUi'
+import FormField from '@/components/ui/form/FormField'
 import IconUi from '@/components/ui/IconUi'
 import MenuUi from '@/components/ui/MenuUi'
 import MessageUi from '@/components/ui/MessageUi'
@@ -8,6 +9,7 @@ import TabsUi from '@/components/ui/TabsUi'
 export {
   BreadcrumbUi,
   ConfirmUi,
+  FormField,
   IconUi,
   MenuUi,
   MessageUi,
