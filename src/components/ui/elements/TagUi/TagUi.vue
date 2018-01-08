141aef4bdf5e4c43da957a474f3a3eb1a33921cf
Luiz Tanure <letanure@gmail.com>
2018-01-08 23:32:40 +0100

    feat: add rules simple tag

--- src/components/ui/elements/TagUi/TagUi.vue
+++ src/components/ui/elements/TagUi/TagUi.vue
@@ -1,16 +1,81 @@
 <template lang="pug">
-  .TitleUi
-    h1.title Title
-    h2.subtitle Subtitle
-
+  span.TagUi.tag(:class='classes', ).
+    {{ text }}
 </template>
 
 <script>
 export default {
-  name: 'TitleUi',
+  name: 'TagUi',
+  props: {
+    color: {
+      default: 'default',
+      type: String,
+      required: false,
+      validator (option) {
+        const options = [
+          'black',
+          'danger',
+          'dark',
+          'default',
+          'info',
+          'light',
+          'link',
+          'primary',
+          'success',
+          'warning',
+          'white',
+        ]
+        return options.includes(option)
+      },
+    },
+
+    text: {
+      default: '',
+      type: String,
+      required: false,
+    },
+
+    size: {
+      default: 'default',
+      type: String,
+      required: false,
+      validator (option) {
+        const options = [
+          'default',
+          'large',
+          'medium',
+        ]
+        return options.includes(option)
+      },
+    },
+
+    rounded: {
+      default: false,
+      type: Boolean,
+      required: false,
+    },
+
+    delete: {
+      default: false,
+      type: Boolean,
+      required: false,
+    },
+  },
+
+  computed: {
+    classes () {
+      const roundedClass = this.rounded ? 'is-rounded' : ''
+      const classes = [
+        `is-${this.color}`,
+        `is-${this.size}`,
+        roundedClass,
+      ]
+      return [...new Set(classes)]
+    },
+  },
 }
 </script>
 
 <style lang="stylus">
-// .TitleUi
+// .TagUi
 </style>
