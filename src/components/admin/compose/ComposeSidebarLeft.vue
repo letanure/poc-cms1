ace17be97be24ae348798c2ad988df653d116d6d
Luiz Tanure <letanure@gmail.com>
2018-01-08 15:45:59 +0100

    feat: load components list in sidebar

--- src/components/admin/compose/ComposeSidebarLeft.vue
+++ src/components/admin/compose/ComposeSidebarLeft.vue
@@ -1,12 +1,12 @@
 <template lang="pug">
   nav.ComposeSidebarLeft
-    .section
-      menu-ui(:groups='menuGroups')
+    .section.is-small
+      menu-ui(:groups='menuGroups', @click='clickComponent')
 
-    //- .menu.animated.slideInLeft
 </template>
 
 <script>
+import { mapGetters } from 'vuex'
 import { MenuUi } from '@/components/ui'
 
 export default {
@@ -16,32 +16,55 @@ export default {
   },
   data () {
     return {
-      menuGroups: [
-        {
-          label: 'General',
-          items: [
-            {
-              text: 'Dashboard',
-              icon: 'tachometer',
-              link: {
-                type: 'router',
-                name: 'DashboardAdmin',
-              },
-            },
-            {
-              text: 'Pages',
-              icon: 'files-o',
-              link: {
-                type: 'router',
-                name: 'PagesList',
-              },
-            },
-          ],
-        },
-      ],
+      menuGroups: [],
     }
   },
+  computed: {
+    ...mapGetters([
+      'componentsList',
+    ]),
+  },
+
+  beforeMount () {
+    this.setMenuBlankComponents()
+    this.setMenuFilledComponents()
+  },
+
   methods: {
+    clickComponent (item) {
+      console.log('clickComponent', item)
+    },
+
+    setMenuBlankComponents () {
+      const menuGroup = {
+        label: 'Blank Components',
+        items: [],
+      }
+      Object
+        .keys(this.componentsList)
+        .filter((componentName) => this.componentsList[componentName].group === 'components')
+        .forEach((componentKey) => {
+          const component = this.componentsList[componentKey]
+          const componentItem = {
+            text: component.label,
+            icon: component.icon,
+            link: {
+              type: 'button',
+              name: ('PagesList' + component.icon),
+            },
+          }
+          menuGroup.items.push(componentItem)
+        })
+      this.menuGroups.push(menuGroup)
+    },
+
+    setMenuFilledComponents () {
+      const menuGroup = {
+        label: 'Filled Components',
+        items: [],
+      }
+      this.menuGroups.push(menuGroup)
+    },
   },
 }
 </script>
@@ -49,8 +72,16 @@ export default {
 <style lang="stylus">
 .ComposeSidebarLeft
   background-color #373737
+  height calc(100vh - 104px)
+
+  .section.is-small
+    padding 1.5rem
+
   .menu-list
     li
       a
+        color #fff
         transition background .2s linear
+        &:hover
+          color #373737
 </style>
