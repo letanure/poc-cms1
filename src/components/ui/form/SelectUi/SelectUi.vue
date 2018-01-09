03fec1886fb03e207bcffb035048100b30a085f7
Luiz Tanure <letanure@gmail.com>
2018-01-09 16:12:09 +0100

    feat: add form component SelectUi

---
+++ src/components/ui/form/SelectUi/SelectUi.vue
@@ -0,0 +1,112 @@
+<template lang="pug">
+  .SelectUi.control
+    .select(:class='classes')
+      select
+        template(v-for='option in options')
+          option()
+            | {{ option.label }}
+</template>
+
+<script>
+
+
+export default {
+  name: 'SelectUi',
+  props: {
+    color: {
+      default: 'default',
+      type: String,
+      required: false,
+      validator (option) {
+        const options = [
+          'default',
+          'primary',
+          'info',
+          'success',
+          'warning',
+          'danger',
+        ]
+        return options.includes(option)
+      },
+    },
+
+    options: {
+      default: () => [],
+      type: Array,
+      required: true,
+      validator (list) {
+        const defaultKeys = ['label']
+        const isValid = list.every((item) => {
+          return isEqual(defaultKeys.sort(), Object.keys(item).sort())
+        })
+        return isValid
+      },
+    },
+
+    multiple: {
+      default: false,
+      type: Boolean,
+      required: false,
+    },
+
+    style: {
+      default: 'default',
+      type: String,
+      required: false,
+      validator (option) {
+        const options = [
+          'default',
+          'rounded',
+        ]
+        return options.includes(option)
+      },
+    },
+
+    size: {
+      default: 'default',
+      type: String,
+      required: false,
+      validator (option) {
+        const options = [
+          'default',
+          'large',
+          'medium',
+          'small',
+        ]
+        return options.includes(option)
+      },
+    },
+
+    state: {
+      default: 'default',
+      type: String,
+      required: false,
+      validator (option) {
+        const options = [
+          'hovered',
+          'focused',
+          'loading',
+        ]
+        return options.includes(option)
+      },
+    },
+  },
+  computed: {
+    classes () {
+      const multipleClass = this.multiple ? 'is-multiple' : ''
+      const classes = [
+        `is-${this.color}`,
+        `is-${this.style}`,
+        `is-${this.size}`,
+        `is-${this.state}`,
+        multipleClass,
+      ]
+      return [...new Set(classes)]
+    },
+  },
+}
+</script>
+
+<style lang="stylus">
+// .SelectUi
+</style>
---
+++ src/components/ui/form/SelectUi/config.js
@@ -0,0 +1,9 @@
+const config = {
+  name: 'SelectUi',
+  label: 'Select',
+  icon: 'wpforms',
+  group: 'form',
+  listable: false,
+}
+
+export default config
