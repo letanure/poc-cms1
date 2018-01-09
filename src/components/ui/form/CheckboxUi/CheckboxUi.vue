4f54235f291d408ff74c722f5ece90546ca7f3e3
Luiz Tanure <letanure@gmail.com>
2018-01-09 16:10:23 +0100

    feat: add form component CheckboxUi

---
+++ src/components/ui/form/CheckboxUi/CheckboxUi.vue
@@ -0,0 +1,29 @@
+<template lang="pug">
+  .CheckboxUi.control
+    label.checkbox(:disabled='disabled', )
+      input(type='checkbox', :disabled='disabled', )
+      span(v-if='label', )
+        | {{ label }}
+</template>
+
+<script>
+export default {
+  name: 'CheckboxUi',
+  props: {
+    label: {
+      default: null,
+      type: String,
+      required: true,
+    },
+    disabled: {
+      default: false,
+      type: Boolean,
+      required: false,
+    },
+  },
+}
+</script>
+
+<style lang="stylus">
+// .CheckboxUi
+</style>
---
+++ src/components/ui/form/CheckboxUi/config.js
@@ -0,0 +1,9 @@
+const config = {
+  name: 'CheckboxUi',
+  label: 'Checkbox',
+  icon: 'wpforms',
+  group: 'form',
+  listable: false,
+}
+
+export default config
