9f39a06d77cbdc767d4c53b77edba683f0376214
Luiz Tanure <letanure@gmail.com>
2018-01-09 16:11:47 +0100

    feat: add form component RadioUi

---
+++ src/components/ui/form/RadioUi/RadioUi.vue
@@ -0,0 +1,39 @@
+<template lang="pug">
+  .RadioUi.control
+    template(v-for='option in options')
+      label.radio
+        input(
+          type='radio',
+          :name='option.name',
+          :checked='option.checked',
+          :disabled='option.disabled',
+          )
+        span
+          | {{ option.label }}
+</template>
+
+<script>
+
+
+export default {
+  name: 'RadioUi',
+  props: {
+    options: {
+      default: () => [],
+      type: Array,
+      required: true,
+      validator (list) {
+        const defaultKeys = ['label', 'name']
+        const isValid = list.every((item) => {
+          return isEqual(defaultKeys.sort(), Object.keys(item).sort())
+        })
+        return isValid
+      },
+    },
+  }
+}
+</script>
+
+<style lang="stylus">
+// .RadioUi
+</style>
---
+++ src/components/ui/form/RadioUi/config.js
@@ -0,0 +1,9 @@
+const config = {
+  name: 'RadioUi',
+  label: 'Radio',
+  icon: 'wpforms',
+  group: 'form',
+  listable: false,
+}
+
+export default config
