907fcd87e4d82e58049fd760f23319f03784ceb2
Luiz Tanure <letanure@gmail.com>
2018-01-09 19:10:51 +0100

    feat: use form components to generate form
    form: {
      title: 'My form',
      fields: [
        // field one
        {
          controls: [
            {
              label: 'Name',
              placeholder: 'Placeholder',
              type: 'text',
            },
          ],
        },
        {
          addons: 'addons',
          controls: [
            {
              placeholder: 'only addon',
              type: 'text',
            },
            {
              type: 'button',
              color: 'link',
              text: 'Submit',
            },
          ],
        },
        {
          addons: 'addons',
          grouped: 'grouped-right',
          controls: [
            {
              placeholder: 'addon and grouped',
              type: 'email',
            },
            {
              type: 'button',
              static: true,
              text: '@gmail.com',
            },
          ],
        },
        {
          addons: 'addons-centered',
          controls: [
            {
              placeholder: 'Your email',
              type: 'email',
            },
            {
              placeholder: 'Placeholder',
              type: 'select',
              options: [
                {
                  label: 'Select dropdown',
                },
                {
                  label: 'With options',
                },
              ],
            },
          ],
        },
        {
          addons: 'addons-right',
          controls: [
            {
              type: 'select',
              options: [
                {
                  label: '$',
                },
                {
                  label: '£',
                },
                {
                  label: '€',
                },
              ],
            },
            {
              placeholder: 'Amount of money',
              type: 'text',
            },
            {
              type: 'button',
              text: 'transfer',
            },
          ],
        },
        {
          grouped: 'grouped',
          controls: [
            {
              placeholder: 'grouped expanded',
              type: 'email',
              color: 'danger',
            },
            {
              type: 'button',
              text: 'transfer',
            },
          ],
        },
        // field 2
        {
          controls: [
            {
              label: 'Username',
              placeholder: 'Placeholder',
              type: 'text',
              color: 'success',
              help: 'This username is available',
            },
          ],
        },
        // field 3
        {
          controls: [
            {
              label: 'Email',
              placeholder: 'Placeholder',
              type: 'email',
              color: 'danger',
              help: 'This email is invalid',
            },
          ],
        },
        // Subject
        {
          controls: [
            {
              label: 'Subject',
              placeholder: 'Placeholder',
              type: 'select',
              options: [
                {
                  label: 'Select dropdown',
                },
                {
                  label: 'With options',
                },
              ],
            },
          ],
        },
        // Message
        {
          controls: [
            {
              label: 'Message',
              placeholder: 'Textarea',
              type: 'textarea',
            },
          ],
        },
        // Message
        {
          controls: [
            {
              label: 'I agree to the terms and conditions',
              type: 'checkbox',
            },
          ],
        },
        // Subject
        {
          controls: [
            {
              label: 'aa',
              type: 'radio',
              options: [
                {
                  label: 'Yes',
                },
                {
                  label: 'No',
                },
              ],
            },
          ],
        },
      ],
      actions: [
        {
          color: 'link',
          text: 'Submit',
        },
        {
          color: 'text',
          text: 'Cancel',
        },
      ],
    },

--- src/components/ui/form/FormUi/FormUi.vue
+++ src/components/ui/form/FormUi/FormUi.vue
@@ -1,12 +1,46 @@
 <template lang="pug">
-  .FormUi
+  form.FormUi
+    h1 {{ title }}
+    template(v-for='field in fields', )
+      field-ui(:field='field', :controls='field.controls', :addons='field.addons', :grouped='field.grouped')
+    .field.is-grouped
+      template(v-for='action in actions', )
+        button-ui(v-bind='action')
 </template>
 
 <script>
-
+import FieldUi from '@/components/ui/form/FieldUi/FieldUi'
+import ButtonUi from '@/components/ui/elements/ButtonUi/ButtonUi'
 
 export default {
   name: 'FormUi',
+  components: {
+    FieldUi,
+    ButtonUi,
+  },
+  props: {
+    title: {
+      default: null,
+      type: String,
+      required: false,
+    },
+
+    fields: {
+      default: () => [],
+      type: Array,
+      required: true,
+    },
+
+    actions: {
+      default: () => [],
+      type: Array,
+      required: true,
+    },
+  },
+  data () {
+    return {
+    }
+  },
 }
 </script>
 
