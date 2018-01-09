b0a8d09064c312333fc240c982443b97067bfba8
Luiz Tanure <letanure@gmail.com>
2018-01-09 16:12:36 +0100

    feat: add form component TextareaUi

---
+++ src/components/ui/form/TextareaUi/TextareaUi.vue
@@ -0,0 +1,104 @@
+<template lang="pug">
+  .TextareaUi.control
+    textarea.textarea(
+      :class='classes',
+      :placeholder='placeholder',
+      :rows='rows',
+      :disabled='disabled',
+      :readonly='readonly',
+    )
+</template>
+
+<script>
+
+
+export default {
+  name: 'TextareaUi',
+  props: {
+    color: {
+      default: 'default',
+      type: String,
+      required: false,
+      validator (option) {
+        const options = [
+          'default',
+          'primary',
+          'info',
+          'success',
+          'warning',
+          'danger',
+        ]
+        return options.includes(option)
+      },
+    },
+
+    placeholder: {
+      default: null,
+      type: String,
+      required: false,
+    },
+
+    rows: {
+      default: 4,
+      type: Number,
+      required: false,
+    },
+
+    size: {
+      default: 'default',
+      type: String,
+      required: false,
+      validator (option) {
+        const options = [
+          'default',
+          'large',
+          'medium',
+          'small',
+        ]
+        return options.includes(option)
+      },
+    },
+
+    disabled: {
+      default: false,
+      type: Boolean,
+      required: false,
+    },
+
+    readonly: {
+      default: false,
+      type: Boolean,
+      required: false,
+    },
+
+    state: {
+      default: 'default',
+      type: String,
+      required: false,
+      validator (option) {
+        const options = [
+          'hovered',
+          'focused',
+          'loading',
+        ]
+        return options.includes(option)
+      },
+    },
+  },
+  computed: {
+    classes () {
+      const staticClass = this.static ? 'is-static' : ''
+      const classes = [
+        `is-${this.color}`,
+        `is-${this.size}`,
+        `is-${this.state}`,
+      ]
+      return [...new Set(classes)]
+    },
+  },
+}
+</script>
+
+<style lang="stylus">
+// .TextareaUi
+</style>
---
+++ src/components/ui/form/TextareaUi/config.js
@@ -0,0 +1,9 @@
+const config = {
+  name: 'TextareaUi',
+  label: 'Textarea',
+  icon: 'wpforms',
+  group: 'form',
+  listable: false,
+}
+
+export default config
