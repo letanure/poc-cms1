5bc11050c4b4f88efb9e9757d523f3dcb7ce2d7f
Luiz Tanure <letanure@gmail.com>
2018-01-18 19:12:13 +0100

    feat: load component config and mock, display form

--- src/components/admin/compose/components/ComponentEdit.vue
+++ src/components/admin/compose/components/ComponentEdit.vue
@@ -1,25 +1,41 @@
 <template lang="pug">
   nav.ComponentEdit
-    h1 editingComponent
-    pre {{editingComponentConfig}}
+    template(v-if='editingComponentConfig', )
+      form-ui(
+        :header='editingComponentConfig.form.header',
+        :fields='editingComponentConfig.form.fields',
+        :actions='editingComponentConfig.form.actions',
+        :value='editingComponentMock',
+        @submit='submit'
+      )
 
 </template>
 
 <script>
 import { mapGetters } from 'vuex'
+import FormUi from '@/components/ui/form/FormUi/FormUi'
 
 export default {
   name: 'ComponentEdit',
+  components: {
+    FormUi,
+  },
   data () {
     return {
+      formData: {},
+      formValues: {},
     }
   },
   computed: {
     ...mapGetters([
       'editingComponentConfig',
+      'editingComponentMock',
     ]),
   },
   methods: {
+    submit () {
+      console.log('submit')
+    },
   },
 }
 </script>
